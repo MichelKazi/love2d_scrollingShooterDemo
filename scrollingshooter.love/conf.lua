242ca534d1c986968f6924135254a424e2268297
Michelkazi <michel.m.kazi@gmail.com>
2020-01-25 21:34:53 -0500

    setup love2d

---
+++ scrollingshooter.love/conf.lua
@@ -0,0 +1,12 @@
+-- Config
+function love.conf(t)
+
+	t.title = "Scrolling Shooter Tutorial"
+	t.version = "0.9.1"
+	t.window.width = 1125
+	t.window.height = 2346
+
+	t.console = true
+end
+
+
---
+++ scrollingshooter.love/main.lua
@@ -0,0 +1,13 @@
+debug = true
+
+function love.load()
+	
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw(dt)
+	
+end
