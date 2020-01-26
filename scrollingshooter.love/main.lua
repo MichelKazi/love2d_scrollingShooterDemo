0c3b5eb4abe37db1e3645d15bbcb7aab14dd36c2
Michelkazi <michel.m.kazi@gmail.com>
2020-01-25 22:01:09 -0500

    oops I forgot to make the plane move vertically

--- scrollingshooter.love/main.lua
+++ scrollingshooter.love/main.lua
@@ -27,6 +27,20 @@ function love.update(dt)
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
 			player.x = player.x + (player.speed*dt)
 		end
+
+
+	end
+
+	if love.keyboard.isDown('up', 'w') then
+		if player.y > 0 then
+			player.y = player.y - (player.speed*dt)
+		end
+	end
+
+	if love.keyboard.isDown('down', 's') then
+		if player.y < (love.graphics.getHeight() - player.img:getHeight()) then
+			player.y = player.y + (player.speed*dt)
+		end
 	end
 
 end
