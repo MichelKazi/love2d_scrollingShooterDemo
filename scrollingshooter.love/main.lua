7c28218de00e4c7cc908138bcdf5f3990d16d4f5
Michelkazi <michel.m.kazi@gmail.com>
2020-01-25 22:26:47 -0500

    bullet functionality added

--- scrollingshooter.love/main.lua
+++ scrollingshooter.love/main.lua
@@ -15,20 +15,23 @@ bulletImg = nil
 function love.load()
 
 	player.img = love.graphics.newImage('assets/plane.png')	
-	bullet.img = love.graphics.newImage('assets/bullet.png')
+	bulletImg = love.graphics.newImage('assets/bullet.png')
 
 end
 
 function love.update(dt)
 
+	-- Exit the game
 	if love.keyboard.isDown('escape') then
 		love.event.push('quit')
 	end
 
+	-- Player movement
+
 	if love.keyboard.isDown('lshift') then
 		player.speed = 300
 	else
-		player.speed = 150
+		player.speed = 100
 	end
 
 	if love.keyboard.isDown('left', 'a') then
@@ -41,8 +44,7 @@ function love.update(dt)
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
 			player.x = player.x + (player.speed*dt)
 		end
-
-
+	
 	end
 
 	if love.keyboard.isDown('up', 'w') then
@@ -57,9 +59,38 @@ function love.update(dt)
 		end
 	end
 
+	-- Timer for bullets
+	canShootTimer = canShootTimer - (1 * dt)
+	if canShootTimer < 0 then
+		canShoot = true
+	end
+
+	-- Remove bullets out of canvas
+	for i, b in ipairs(bullet) do
+		if b.y < -10 then
+			table.remove(bullet, i)
+		end
+		b.y = b.y - 150
+	end
+
+	-- Player attacks
+	if love.keyboard.isDown('space', 'lctrl') and canShoot then
+		newBullet = {
+			x = player.x+(player.img:getWidth()/2),
+			y = player.y,
+		}
+		table.insert(bullet, newBullet)
+		img = bulletImg
+		canShoot = false
+		canShootTimer = canShootTimerMax
+	end
+
 end
 
 function love.draw(dt)
 	love.graphics.draw(player.img, player.x, player.y)
+	for i,v in pairs(bullet) do 
+		love.graphics.draw(bulletImg, v.x, v.y)
+	end
 
 end
